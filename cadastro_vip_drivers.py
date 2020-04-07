3dcb2ece543db4e8167fdeccf6da23b57880ebd0
Isaque Lourenço <isaque.lourenco2@gmail.com>
2020-04-07 16:35:07 -0300

    Inserindo dados em tabelas multiplas

--- cadastro_vip_drivers.py
+++ cadastro_vip_drivers.py
@@ -4,7 +4,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
-class Usuario():
+class Usuario_dados_parceiro():
     def __init__(self, id, nome, tel, email, indicado):
         self.id = id
         self.nome = nome
@@ -12,12 +12,41 @@ class Usuario():
         self.email = email
         self.indicado = indicado
 
+class Usuario_endereco_parceiro():
+    def __init__(self, rua, numero, bairro, cidade):
+        self.rua = rua
+        self.numero = numero
+        self.bairro = bairro
+        self.cidade = cidade
+
+
+class Usuario_dados_carro():
+    def __init__(self, modelo, cor, placa):
+        self.modelo = modelo
+        self.cor = cor
+        self.placa = placa
+
+class Usuario_contato_familiar():
+    def __init__(self, nome_familiar, tel_familiar):
+        self.nome_familiar = nome_familiar
+        self.tel_familiar = tel_familiar
+
 
 class Controlador(object):
     def __init__(self):
-        self.modelo_armazenamento: Gtk.ListStore = Builder.get_object("dados_parceiro")
         self.Stack = Builder.get_object("stack")
-        self.banco_dados = []
+
+        self.modelo_armazenamento_dados_parceiro: Gtk.ListStore = Builder.get_object("dados_parceiro")
+        self.modelo_armazenamento_endereco_parceiro: Gtk.ListStore = Builder.get_object("endereco_parceiro")
+        self.modelo_armazenamento_dados_carro: Gtk.ListStore = Builder.get_object("dados_carro")
+        self.modelo_armazenamento_dados_contato_familiar: Gtk.ListStore = Builder.get_object("contato_familiar")
+
+        self.banco_dados_parceiro = []
+        self.banco_dados_endereco_parceiro = []
+        self.banco_dados_carro = []
+        self.banco_dados_contato_familiar = []
+
+
 
     def on_tela_principal_destroy(self, window):
         Gtk.main_quit()
@@ -48,6 +77,7 @@ class Controlador(object):
     def on_button_cadastrar_clicked(self, button):
         self.Stack.set_visible_child_name("tela_cadastro")
 
+
     def on_button_listar_inicial_clicked(self, button):
         self.Stack.set_visible_child_name("tela_listar")
 
@@ -61,31 +91,69 @@ class Controlador(object):
         self.Stack.set_visible_child_name("tela_inicial")
 
     def on_button_lista_clicked(self, button):
-        self.modelo_armazenamento.clear()
-        for Usuario in self.banco_dados:
-            self.modelo_armazenamento.append((Usuario.id, Usuario.nome, Usuario.tel, Usuario.email, Usuario.indicado))
+        self.modelo_armazenamento_dados_parceiro.clear()
+        self.modelo_armazenamento_endereco_parceiro.clear()
+        self.modelo_armazenamento_dados_carro.clear()
+        self.modelo_armazenamento_dados_contato_familiar.clear()
+
+        for Usuario_dados_parceiro in self.banco_dados_parceiro:
+                self.modelo_armazenamento_dados_parceiro.append((Usuario_dados_parceiro.id,
+                                              Usuario_dados_parceiro.nome,
+                                              Usuario_dados_parceiro.tel,
+                                              Usuario_dados_parceiro.email,
+                                              Usuario_dados_parceiro.indicado))
+
+        for Usuario_endereco_parceiro in self.banco_dados_endereco_parceiro:
+                self.modelo_armazenamento_endereco_parceiro.append((Usuario_endereco_parceiro.rua,
+                                              Usuario_endereco_parceiro.numero,
+                                              Usuario_endereco_parceiro.bairro,
+                                              Usuario_endereco_parceiro.cidade))
+
+
+        for Usuario_dados_carro in self.banco_dados_carro:
+                self.modelo_armazenamento_dados_carro.append((Usuario_dados_carro.modelo,
+                                              Usuario_dados_carro.cor,
+                                              Usuario_dados_carro.placa))
+
+        for Usuario_contato_familiar in self.banco_dados_contato_familiar:
+                self.modelo_armazenamento_dados_contato_familiar.append((Usuario_contato_familiar.nome_familiar,
+                                              Usuario_contato_familiar.tel_familiar))
+
+        print(self.banco_dados_parceiro)
+
+
 
     def on_button_cad_cadastrar_clicked(self, button):
+
         nome = Builder.get_object("nome_cad").get_text()
         tel = Builder.get_object("tel_cad").get_text()
         email = Builder.get_object("email_cad").get_text()
         indicado = Builder.get_object("indicado_cad").get_text()
+
         rua = Builder.get_object("rua_cad").get_text()
         numero = Builder.get_object("num_cad").get_text()
         bairro = Builder.get_object("bairro_cad").get_text()
         cidade = Builder.get_object("cidade_cad").get_text()
-        nome_fam = Builder.get_object("nome_cad_familiar").get_text()
-        tel_fam = Builder.get_object("tel_cad_familiar").get_text()
-        modelo = Builder.get_object("modelo_cad").get_text()
+
         cor = Builder.get_object("cor_cad").get_text()
         placa = Builder.get_object("placa_cad").get_text()
+        modelo = Builder.get_object("modelo_cad").get_text()
+
+        nome_fam = Builder.get_object("nome_cad_familiar").get_text()
+        tel_fam = Builder.get_object("tel_cad_familiar").get_text()
 
         if nome != '':
-            self.banco_dados.append(Usuario(len(self.banco_dados)+1, nome, tel, email, indicado))
+            self.banco_dados_parceiro.append(Usuario_dados_parceiro(len(self.banco_dados_parceiro) + 1,
+                                                                    nome, tel, email, indicado))
+            self.banco_dados_endereco_parceiro.append(Usuario_endereco_parceiro(rua, numero, bairro, cidade))
+            self.banco_dados_carro.append(Usuario_dados_carro(modelo, cor, placa))
+            self.banco_dados_contato_familiar.append(Usuario_contato_familiar(nome_fam, tel_fam))
+
             self.mensagem('Aviso', 'Usuário ' + nome + ' cadastrado!', 'dialog-emblem-default')
+
+
         else:
             self.mensagem('Aviso', 'Campo "Nome" Obrigatório!', 'dialog-error')
-
 
 
 
